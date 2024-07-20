b00684edbacb1697f9846a24b73b9b0a0f184c9d
iayush0verma12 <91520907+iayush0verma12@users.noreply.github.com>
2024-07-21 04:13:37 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,6 @@ class MyDocument extends Document<DocumentProps> {
     return (
       <Html lang="en">
         <Head>
-          <meta charSet="utf-8" />
           <link rel="icon" href="/favicon.ico" />
           <meta name="viewport" content="initial-scale=1, width=device-width" />
 
