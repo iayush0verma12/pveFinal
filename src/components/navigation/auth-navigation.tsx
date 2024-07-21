05a29aa89c0aa7c07dab6e83b9e5c4f08a880bb5
iayush0verma12 <91520907+iayush0verma12@users.noreply.github.com>
2024-07-21 18:30:15 +0530

    Update auth-navigation.tsx

--- src/components/navigation/auth-navigation.tsx
+++ src/components/navigation/auth-navigation.tsx
@@ -1,66 +1,16 @@
-import React, { FC, useState } from 'react';
-import Box from '@mui/material/Box';
-import { StyledButton } from '@/components/styled-button';
-import Typography from '@mui/material/Typography';
-import Image from 'next/image';
+import React, { FC } from 'react'
+import Box from '@mui/material/Box'
+import { StyledButton } from '@/components/styled-button'
 
 const AuthNavigation: FC = () => {
-  const [showPhoneNumber, setShowPhoneNumber] = useState(false);
-
-  const handleButtonClick = () => {
-    setShowPhoneNumber(!showPhoneNumber);
-  };
-
   return (
     <Box sx={{ '& button:first-child': { mr: 2 } }}>
-      <StyledButton disableHoverEffect={true} onClick={handleButtonClick}>
-        {showPhoneNumber ? 'Hide' : 'CALL NOW'}
+      <StyledButton disableHoverEffect={true} variant="outlined">
+        Sign In
       </StyledButton>
-      {showPhoneNumber && (
-        <Box sx={{ mt: 2 }}>
-          <Typography variant="body1">
-            Know more about our products. Contact Now!
-          </Typography>
-          <Box display="flex" alignItems="center">
-            <Image
-              src="/images/contact.png"
-              alt="Logo"
-              width={20}
-              height={20}
-              style={{ marginRight: 8 }}
-            />
-            <Typography variant="body1">
-              Phone Number: +91-7033664425
-            </Typography>
-          </Box>
-          {/* <Box display="flex" alignItems="center">
-            <img
-              src="/images/alternate.png"
-              alt="Logo"
-              width={20}
-              height={20}
-              style={{ marginRight: 8 }}
-            />
-            <Typography variant="body1">
-              Alternate Number: 7033664425
-            </Typography>
-          </Box> */}
-          <Box display="flex" alignItems="center">
-            <Image
-              src="/images/email.png"
-              alt="Logo"
-              width={20}
-              height={20}
-              style={{ marginRight: 8 }}
-            />
-            <Typography variant="body1">
-              Email: premvishalenterprises@gmail.com
-            </Typography>
-          </Box>
-        </Box>
-      )}
+      <StyledButton disableHoverEffect={true}>Sign Up</StyledButton>
     </Box>
-  );
-};
+  )
+}
 
-export default AuthNavigation;
+export default AuthNavigation
